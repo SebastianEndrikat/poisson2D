edcd6ceb537e80dce26190805c683b040da2d0af
Sebastian <sebastian.e@live.ca>
2020-09-08 23:57:59 +0000

    added interpolation version

--- meshlessFish.py
+++ meshlessFish.py
@@ -102,6 +102,60 @@ def minimizeWeights(Afull,Wfull,x0,y0):
         theDet = np.linalg.det(np.matmul(np.matmul(amat.T,weight),amat))
     return amat,weight,Keff
 
+def interpolate(y,z,f,ynew,znew,yPeriodic=0.0,K=31,R=None,verbose=True):
+    # find zeroth derivatives of f at every point in a point cloud
+    # y,z,f are 1D arrays of the known field
+    # ynew, znew are 1D arrays of the field to which we interpolate
+    # if yPeriodic>0., points will be copied by that distance in y
+    # K is the max number of neighbors. Will use fewest possible tho
+    
+    K=int(K)
+    Nf=len(f)
+    Nnew=len(ynew)
+    
+    if yPeriodic>0.0:
+        ycut=np.min(y)+(yPeriodic/2.)
+        sel0=(y<ycut)
+        sel1=(y>ycut)
+        
+        y=np.append(np.append(y,y[sel0]+yPeriodic),y[sel1]-yPeriodic)
+        z=np.append(np.append(z,z[sel0]),z[sel1])
+        f=np.append(np.append(f,f[sel0]),f[sel1])
+        
+    tree = KDTree( zip( y, z ) )  # build the tree
+    # query tree and get distances and integers of K points near every point:
+    queries = tree.query(zip(ynew,znew) , K) 
+    KInds = queries[1] # the integers, distances are in [0]. matrix of Nf by K indices
+    
+    
+    # get R
+    if np.any(R==None):
+        t0=time.time()
+        R=np.zeros((Nnew,3,K))
+        maxmaxK=0
+        for ip in range(Nnew): # only over the points for which I wanna find the deriv
+            I=KInds[ip,:] # indeces of the K neighbor points
+            A,W=getAnW_firstDerivs(y[I],z[I],ynew[ip],znew[ip])
+            A,W,Keff=minimizeWeights(A,W,ynew[ip],znew[ip])
+            maxmaxK=np.max([maxmaxK,Keff])
+            R[ip,:,:]=scipy.matmul(scipy.matmul(scipy.linalg.inv(scipy.matmul(scipy.matmul(A.T,W),A)),A.T),W)
+        t1=time.time()
+        if verbose:
+            print('meshlessFish.interpolate: Got R in %.8f seconds. Highest number of neighbors used anywhere: %i' %((t1-t0),maxmaxK) )
+            
+    # assamble the known part of the system
+    known=np.zeros((Nnew,K,1))
+    for ip in range(Nnew): # only over the points for which I wanna find the deriv
+        I=KInds[ip,:] # indeces of the K neighbor points
+        known[ip,:,0]=f[I]
+        
+        
+    t0=time.time()
+    out=np.matmul(R,known) # elementise matrix multiplication. matrices are in the last dimensions
+    fnew = np.squeeze(out[:,0])
+        
+    return fnew
+
 def getFirstDerivs(y,z,f,yPeriodic=0.0,K=31,R=None,verbose=True):
     # find first derivatives of f at every point in a point cloud
     # y,z,f are 1D arrays
@@ -209,7 +263,9 @@ def iteratePoisson(f,knownConst,R,KInds,ghostMap,Nf,K,alpha,maxit,TOL,verbose):
         res=np.abs(fmax-fmax0)/fmax
         if verbose:
             if np.mod(ll,100)==0:
-                printReplace('ll=%i, residual = %.6e' %(ll,res))
+#                printReplace('ll=%i, residual = %.6e' %(ll,res))
+                avgf=np.mean(f) # not a proper integral tho!
+                printReplace('ll=%i, residual = %.6e, avgf=%.6e' %(ll,res,avgf))
     if verbose:
         printReplace('',done=True)
     return f,res
